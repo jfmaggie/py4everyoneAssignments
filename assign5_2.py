b8a4cf81bd904600ec187673a3d4344d26d1d4f7
Meichen Zhou <jfmaggie@gmail.com>
2014-12-04 13:55:15 -0800

    Update assign5_2.py

--- assign5_2.py
+++ assign5_2.py
@@ -1,6 +1,14 @@
+'''
+5.2 Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. Once 'done' is entered, print out the largest and smallest of the numbers. If the user enters anything other than a valid number catch it with a try/except and put out an appropriate message and ignore the number. Enter the numbers from the book for problem 5.1 and Match the desired output as shown.
+
+Output:
+Invalid input
+Maximum is 7
+Minimum is 4
+'''
+
 largest = None
 smallest = None
-
 
 while True:
     num = raw_input("Enter a number: ")
@@ -17,7 +25,6 @@ while True:
         largest = value
     elif value <= smallest:
         smallest = value
-       
 
 print "Maximum is", largest
 print "Minimum is", smallest

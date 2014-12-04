5ccc68f05fd7d2150c0227e6989f61eccf698b14
Meichen Zhou <jfmaggie@gmail.com>
2014-12-04 13:58:28 -0800

    Update assign3_1.py

--- assign3_1.py
+++ assign3_1.py
@@ -1,3 +1,10 @@
+'''
+3.1 Write a program to prompt the user for hours and rate per hour using raw_input to compute gross pay. Pay the hourly rate for the hours up to 40 and 1.5 times the hourly rate for all hours worked above 40 hours. Use 45 hours and a rate of 10.50 per hour to test the program (the pay should be 498.75). You should use raw_input to read a string and float() to convert the string to a number. Do not worry about error checking the user input - assume the user types numbers properly.
+
+Output:
+498.75
+'''
+
 hrs = raw_input("Enter Hours:")
 h = float(hrs)
 rate = raw_input("Enter Rate:")
@@ -5,6 +12,5 @@ r = float(rate)
 
 if (h <= 40):
 	print "Gross pay: ", h * r
-   
 else:
     print  r * 40 + (h - 40) * 1.5 *r
